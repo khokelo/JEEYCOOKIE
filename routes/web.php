aaa33f8dc730548b788d68596e55aa12f78d4ec8
Nathan Delvian Anggara <Natzen0z@users.noreply.github.com>
2025-09-30 21:23:16 +0700

    Nih buat Routes, cuman pagesnya belum ada

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,27 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Home
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home');
+})->name('home');
+
+// About
+Route::get('/about', function () {
+    return view('about');
+})->name('about');
+
+// Program
+Route::get('/program', function () {
+    return view('program');
+})->name('program');
+
+// Our Team
+Route::get('/our-team', function () {
+    return view('our-team');
+})->name('our-team');
+
+// Contact Us
+Route::get('/contact-us', function () {
+    return view('contact-us');
+})->name('contact-us');
