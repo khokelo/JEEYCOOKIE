1ea66b426dd0fdc9129ea733825bc27c111b9da8
Nathan Delvian Anggara <Natzen0z@users.noreply.github.com>
2025-10-01 15:17:45 +0700

    NavBar (updated)

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,3 +1,11 @@
+<nav>
+  <a href="{{ route('home') }}">Home</a>
+  <a href="{{ route('about') }}">About</a>
+  <a href="{{ route('program') }}">Program</a>
+  <a href="{{ route('our-team') }}">Our Team</a>
+  <a href="{{ route('contact-us') }}">Contact Us</a>
+</nav>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -9,3 +17,35 @@
        In ac purus a lorem faucibus malesuada nec at sapien.</p>
 </body>
 </html>
+
+<?php
+use Illuminate\Support\Facades\Route;
+
+// Home
+Route::get('/', function () {
+    return view('home');
+})->name('home');
+
+// About
+Route::get('/about', function () {
+    return view('about');
+})->name('about');
+
+// Program
+Route::get('/program', function () {
+    return view('program');
+})->name('program');
+
+// Our Team
+Route::get('/our-team', function () {
+    return view('our-team');
+})->name('our-team');
+
+// Contact Us
+Route::get('/contact-us', function () {
+    return view('contact-us');
+})->name('contact-us');
+?>
+
+
+
--- resources/views/our-team.blade.php
+++ resources/views/our-team.blade.php
@@ -1,3 +1,41 @@
+<nav>
+  <a href="{{ route('home') }}">Home</a>
+  <a href="{{ route('about') }}">About</a>
+  <a href="{{ route('program') }}">Program</a>
+  <a href="{{ route('our-team') }}">Our Team</a>
+  <a href="{{ route('contact-us') }}">Contact Us</a>
+</nav>
+
+<?php
+use Illuminate\Support\Facades\Route;
+
+// Home
+Route::get('/', function () {
+    return view('home');
+})->name('home');
+
+// About
+Route::get('/about', function () {
+    return view('about');
+})->name('about');
+
+// Program
+Route::get('/program', function () {
+    return view('program');
+})->name('program');
+
+// Our Team
+Route::get('/our-team', function () {
+    return view('our-team');
+})->name('our-team');
+
+// Contact Us
+Route::get('/contact-us', function () {
+    return view('contact-us');
+})->name('contact-us');
+?>
+
+
 <!DOCTYPE html>
 <html>
 <head>
